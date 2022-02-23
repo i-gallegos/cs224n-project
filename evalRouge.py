a148f29706b38d77f9e6150c6f9ffa9b5ae8e1e9
kayleegeorge <krgeorge@stanford.edu>
2022-02-22 16:52:40 -0800

    update evalRouge

--- evalRouge.py
+++ evalRouge.py
@@ -7,12 +7,37 @@ For each of these, we receive the F1 score f, precision p, and recall r.]
 """
 
 from rouge import Rouge
+import argparse
 
-# load in model_output and reference with appropriate files
-model_output = ["string"]
-reference = ["str"]
+# outputs rouge scores
+def eval(model_file, ref_file):
+    # get text from given files
+    with open(model_file, "r") as mf:
+        model_txt = mf.read()
+    with open(ref_file, "r") as rf:
+        ref_txt = rf.read()
 
-rouge = Rouge()
-scores = rouge.get_scores(model_output, reference, avg=True)
-print("Rouge scores: ")
-print(scores)
+    assert(len(model_txt)) != 0
+    assert(len(ref_txt)) != 0
+
+    model_txt = model_txt.split('\n')
+    ref_txt = ref_txt.split('\n')
+
+    # compute rouge scores
+    rouge = Rouge()
+    scores = rouge.get_scores(model_txt, ref_txt, avg=True)
+    print("Rouge scores: ")
+    print(scores)
+    return scores
+
+# driver code: parse model_output and reference 
+def eval_rogue(args):
+    argp = argparse.ArgumentParser()
+    argp.add_argument('model_output', help="predicted summarized sentences model outputs")
+    argp.add_argument('reference_text', help = "reference summarized sentences")
+    args = argp.parse_args()
+    model_file = args.model_output
+    ref_file = args.reference_text
+
+    # run rouge evaluation
+    eval(model_file, ref_file)

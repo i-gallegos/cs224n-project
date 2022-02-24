bb727659a80b8f439a08aaadf7ffb0b2f05e140e
Isabel Gallegos <iogalle@stanford.edu>
2022-02-23 20:33:44 -0800

    Add custom dataloader

--- bart.py
+++ bart.py
@@ -5,6 +5,29 @@ from transformers import TrainerCallback, EarlyStoppingCallback
 from tqdm.notebook import tqdm
 from torch.utils.data import DataLoader
 
+# Download the model
+model_name = "facebook/bart-large-cnn"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+BATCH_SIZE = 16
+
+class CustomDataset(Dataset):
+  def __init__(self,file_name):
+    df=pd.read_csv(file_name)
+
+    x=df['original_text'].values
+    y=df['reference_summary'].values
+
+    self.x=x
+    self.y=y
+
+  def __len__(self):
+    return len(self.y)
+
+  def __getitem__(self,idx):
+    return self.x[idx],self.y[idx]
+
+
 class LoggingCallback(TrainerCallback):
     def __init__(self, log_path):
         self.log_path = log_path
@@ -22,22 +45,27 @@ def compute_metrics(eval_pred):
     # calculates the accuracy
     return {"accuracy": np.mean(predictions == labels)}
 
-# Download the model
-model_name = "facebook/bart-large-cnn"
-tokenizer = AutoTokenizer.from_pretrained(model_name)
+def create_dataloaders(train_path, dev_path, test_path):
+    train_dataset = CustomDataset(train_path)
+    train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=False)
 
-# Create dataloaders
-# TODO
+    dev_dataset = CustomDataset(dev_path)
+    dev_loader = DataLoader(dev_dataset, batch_size=BATCH_SIZE, shuffle=False)
 
-def train():
+    test_dataset = CustomDataset(test_path)
+    test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False)
+
+    return train_loader, dev_loader, test_loader
+
+def train(train_dataset, dev_dataset):
     # Load model
     model = AutoModel.from_pretrained(model_name)
 
     # Fine-tuning parameters
     arguments = TrainingArguments(
         output_dir="trainer",
-        per_device_train_batch_size=16,
-        per_device_eval_batch_size=16,
+        per_device_train_batch_size=BATCH_SIZE,
+        per_device_eval_batch_size=BATCH_SIZE,
         num_train_epochs=3,
         evaluation_strategy="epoch", # run validation at the end of each epoch
         save_strategy="epoch",
@@ -50,8 +78,8 @@ def train():
     trainer = Trainer(
         model=model,
         args=arguments,
-        train_dataset=small_tokenized_dataset['train'],
-        eval_dataset=small_tokenized_dataset['val'], # change to test when you do your final evaluation!
+        train_dataset=train_dataset,
+        eval_dataset=eval_dataset,
         tokenizer=tokenizer,
         compute_metrics=compute_metrics
     )
@@ -70,11 +98,18 @@ def test(test_text):
 
     # TODO: change for summary task
     prediction = torch.argmax(finetuned_model(**model_inputs).logits)
-    print(["NEGATIVE", "POSITIVE"][prediction])
+
 
 def main():
-    train()
-    test()
+    dataset = 'tosdr'
+    train_path = os.path.join('data', dataset, dataset+'_train.csv')
+    dev_path = os.path.join('data', dataset, dataset+'_dev.csv')
+    test_path = os.path.join('data', dataset, dataset+'_test.csv')
+    train_loader, dev_loader, test_loader = create_dataloaders(train_path, dev_path, test_path)
+
+    train(train_loader, dev_loader)
+    # test()
+
 
 if __name__ == "__main__":
     main()

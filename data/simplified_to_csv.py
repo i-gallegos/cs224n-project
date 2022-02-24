9e86d6b5dd04b0681412cbcf66d937b727a3d532
Isabel Gallegos <iogalle@stanford.edu>
2022-02-23 18:53:21 -0800

    Create datasets for simplified input

---
+++ data/simplified_to_csv.py
@@ -0,0 +1,15 @@
+import os
+import pandas as pd
+
+simplified_dir = '../access/access/preds'
+
+for dataset in ['tldr', 'tosdr']:
+    for split in ['test']: #['train', 'dev', 'test']:
+        filepath = os.path.join(simplified_dir, 'preds_'+dataset+'_'+split)
+        with open(filepath) as file:
+            simplified = [line.rstrip() for line in file]
+
+        df = pd.read_csv(os.path.join(dataset, dataset+'_'+split+'.csv'))
+        df = df.drop(columns=['original_text'])
+        df['original_text'] = simplified
+        df.to_csv(os.path.join(dataset, dataset+'_'+split+'_simplified.csv'), index=False)

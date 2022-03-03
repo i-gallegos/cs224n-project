4e04ce8ca90cf38fb8b371d56e1520207c8baa8d
kayleegeorge <krgeorge@stanford.edu>
2022-03-02 21:17:04 -0800

    merge

--- run.py
+++ run.py
@@ -5,4 +5,5 @@ seeds = ['224', '161']
 for dataset in datasets:
     for lr in lrs:
         for seed in seeds:
-            os.system(f"python fine_tune_bart.py {dataset} {lr} {seed} --batch_size 32 --grad_accumulation_steps 4 --train_on_full_dataset False")
+            os.system(f"python fine_tune_bart.py {dataset} {lr} {seed} --batch_size 16 --grad_accumulation_steps 8 --train_on_full_dataset True")
+            os.system(f'rm -r bart-large-cnn-finetuned')

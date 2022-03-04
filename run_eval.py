31e4674adc4c127358d92a61edbee0f296b04885
Isabel Gallegos <iogalle@stanford.edu>
2022-03-03 21:23:53 -0800

    Update run eval

--- run_eval.py
+++ run_eval.py
@@ -1,2 +1,13 @@
 import os
-os.system(f"python evaluate_bart.py TODOOOOO")
+'''EXAMPLE
+dataset = tldr
+name = tldr_test
+experiment = TODO
+checkpoint = checkpoint-1
+
+os.system(f"python evaluate_bart.py bart-large-cnn-finetuned/{checkpoint}/ \
+            --test_file data/{dataset}/{name}.csv \
+            --out_file results/{experiment}/preds_{name}.txt \
+            --ref_file data/reference/{name}_ref.txt \
+            --result_file results/{experiment}/rouge_{name}.csv")
+'''

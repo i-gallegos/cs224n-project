3472d5f796336089b98edc05657ccd3f340b0215
kayleegeorge <krgeorge@stanford.edu>
2022-03-03 21:30:42 -0800

    add ref full

--- data/csv_to_text.py
+++ data/csv_to_text.py
@@ -5,16 +5,15 @@ def read_csv(dataset):
     cur_path = os.getcwd() 
     dataset_path = f'{cur_path}/../data/{dataset}'
     phases = ['test', 'train', 'dev']
-    for phase in phases:
-        csv_file = f'{dataset_path}/{dataset}_{phase}.csv'
-        with open(csv_file, 'r') as input_file:
-            col_list = ['document', 'summary', 'id']
-            df = pd.read_csv(input_file, usecols=col_list)
-
-        # 
-        with open(f'reference_{dataset}_full.txt', "w") as ref_file:
-            for row in df['summary']:
-                ref_file.write("".join(row))
+    with open(f'reference_{dataset}_full.txt', "w") as ref_file:
+        for phase in phases:
+            csv_file = f'{dataset_path}/{dataset}_{phase}.csv'
+            with open(csv_file, 'r') as input_file:
+                col_list = ['document', 'summary', 'id']
+                df = pd.read_csv(input_file, usecols=col_list)
+                
+                for row in df['summary']:
+                    ref_file.write("".join(row + '\n'))
     ref_file.close()
     return f'reference_{dataset}_full.txt'
 

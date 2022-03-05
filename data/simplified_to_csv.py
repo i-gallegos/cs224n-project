c8e185e2e642432b5081bd429782f64986f7fb8d
Isabel Gallegos <iogalle@stanford.edu>
2022-03-04 17:41:07 -0800

    Fix bug in simplified small billsum

--- data/simplified_to_csv.py
+++ data/simplified_to_csv.py
@@ -8,8 +8,10 @@ for dataset in ['small_billsum']:
         filepath = os.path.join(simplified_dir, dataset, 'preds_'+dataset+'_'+split)
         with open(filepath) as file:
             simplified = [line.rstrip() for line in file]
+            print(simplified[0])
 
         df = pd.read_csv(os.path.join(dataset, dataset+'_'+split+'.csv'))
         df = df.drop(columns=['summary'])
         df['summary'] = simplified
+        print(df['summary'])
         df.to_csv(os.path.join(dataset, dataset+'_'+split+'_simplified.csv'), index=False)

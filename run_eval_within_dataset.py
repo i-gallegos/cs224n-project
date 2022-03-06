5117d7b5c3ebeea9c366b9c33b4982db3eba101c
i-gallegos <iogalle@stanford.edu>
2022-03-06 09:42:34 +0000

    Eval

--- run_eval_within_dataset.py
+++ run_eval_within_dataset.py
@@ -1,16 +1,18 @@
 import os
 
 experiment = 'within_dataset'
-'''
+
+print('tldr')
 dataset = 'tldr'
 name = 'tldr_test'
-checkpoint = 'tldr_lr2e-05_seed224_full_datasetFalse/checkpoint-4'
+checkpoint = 'tldr_lr3e-05_seed161_full_datasetFalse/checkpoint-4'
 os.system(f"python evaluate_bart.py bart-large-cnn-finetuned/{checkpoint}/ \
             --test_file data/{dataset}/{dataset}_test.csv \
             --out_file results/{experiment}/preds_{name}.txt \
             --ref_file data/reference/reference_{dataset}_test.txt \
             --result_file results/{experiment}/rouge_{name}.csv")
 
+print('tosdr')
 dataset = 'tosdr'
 name = 'tosdr_test'
 checkpoint = 'tosdr_lr3e-05_seed161_full_datasetFalse/checkpoint-8'
@@ -19,10 +21,21 @@ os.system(f"python evaluate_bart.py bart-large-cnn-finetuned/{checkpoint}/ \
             --out_file results/{experiment}/preds_{name}.txt \
             --ref_file data/reference/reference_{dataset}_test.txt \
             --result_file results/{experiment}/rouge_{name}.csv")
-'''
+
+print('small_billsum')
 dataset = 'small_billsum'
 name = 'small_billsum_test'
-checkpoint = 'small_billsum_lr3e-05_seed224_full_datasetFalse/checkpoint-52'
+checkpoint = 'small_billsum_lr3e-05_seed161_full_datasetFalse/checkpoint-44'
+os.system(f"python evaluate_bart.py bart-large-cnn-finetuned/{checkpoint}/ \
+            --test_file data/{dataset}/{dataset}_test.csv \
+            --out_file results/{experiment}/preds_{name}.txt \
+            --ref_file data/reference/reference_{dataset}_test.txt \
+            --result_file results/{experiment}/rouge_{name}.csv")
+
+print('tiny_billsum')
+dataset = 'tiny_billsum'
+name = 'tiny_billsum_test'
+checkpoint = 'tiny_billsum_lr3e-05_seed161_full_datasetFalse/checkpoint-4'
 os.system(f"python evaluate_bart.py bart-large-cnn-finetuned/{checkpoint}/ \
             --test_file data/{dataset}/{dataset}_test.csv \
             --out_file results/{experiment}/preds_{name}.txt \
--- run_final_within_dataset.py
+++ run_final_within_dataset.py
@@ -1,22 +1,17 @@
 import os
-'''
-os.system(f"python fine_tune_bart.py tldr 0.00002 224 \
+
+os.system(f"python fine_tune_bart.py tldr 0.00003 161 \
             --batch_size 16 \
-            --grad_accumulation_steps 8 \
-            --train_on_full_dataset False")
+            --grad_accumulation_steps 8")
 
 os.system(f"python fine_tune_bart.py tosdr 0.00003 161 \
             --batch_size 16 \
-            --grad_accumulation_steps 8 \
-            --train_on_full_dataset False")
+            --grad_accumulation_steps 8")
 
-os.system(f"python fine_tune_bart.py small_billsum 0.00003 224 \
+os.system(f"python fine_tune_bart.py small_billsum 0.00003 161 \
             --batch_size 16 \
-            --grad_accumulation_steps 8 \
-            --train_on_full_dataset False")
-'''
+            --grad_accumulation_steps 8")
 
-os.system(f"python fine_tune_bart.py tiny_billsum 0.00003 224 \
+os.system(f"python fine_tune_bart.py tiny_billsum 0.00003 161 \
             --batch_size 16 \
-            --grad_accumulation_steps 8 \
-            --train_on_full_dataset False")
+            --grad_accumulation_steps 8")

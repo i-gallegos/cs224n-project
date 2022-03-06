4b6d12bbc06a151f745a552077a5c17fb6a14d48
Isabel Gallegos <iogalle@stanford.edu>
2022-03-05 20:06:31 -0800

    Billsum preds

--- run_eval_within_dataset.py
+++ run_eval_within_dataset.py
@@ -1,7 +1,7 @@
 import os
 
 experiment = 'within_dataset'
-
+'''
 dataset = 'tldr'
 name = 'tldr_test'
 checkpoint = 'tldr_lr2e-05_seed224_full_datasetFalse/checkpoint-4'
@@ -19,7 +19,7 @@ os.system(f"python evaluate_bart.py bart-large-cnn-finetuned/{checkpoint}/ \
             --out_file results/{experiment}/preds_{name}.txt \
             --ref_file data/reference/reference_{dataset}_test.txt \
             --result_file results/{experiment}/rouge_{name}.csv")
-
+'''
 dataset = 'small_billsum'
 name = 'small_billsum_test'
 checkpoint = 'small_billsum_lr3e-05_seed224_full_datasetFalse/checkpoint-52'

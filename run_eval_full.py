f765da6b11ba36f0f254812b5ed5f09801a1e983
kayleegeorge <krgeorge@stanford.edu>
2022-03-06 23:23:38 -0800

    more full_dataset

--- run_eval_full.py
+++ run_eval_full.py
@@ -1,7 +1,7 @@
 import os
 
 experiment = 'full_dataset'
-
+"""
 datasets = ['tosdr', 'small_billsum']
 name = 'tldr_full'
 checkpoint = 'tldr_lr3e-05_seed161_full_datasetTrue/checkpoint-4'
@@ -11,8 +11,10 @@ for dataset in datasets:
             --out_file results/{experiment}/preds_{name}_on_{dataset}.txt \
             --ref_file data/reference/reference_{dataset}_test.txt \
             --result_file results/{experiment}/rouge_{name}_on_{dataset}.csv")
+"""
 
-datasets = ['tldr', 'small_billsum']
+#datasets = ['tldr', 'small_billsum']
+datasets = ['small_billsum']
 name = 'tosdr_full'
 checkpoint = 'tosdr_lr3e-05_seed161_full_datasetTrue/checkpoint-8'
 for dataset in datasets:

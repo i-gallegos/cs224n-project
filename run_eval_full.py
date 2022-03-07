757470a845a6c6d04a886f8fd45f7f6837eb4544
Ubuntu <krgeorge@cs224n-kg.4fvhwc23f3budffy5qtr44bz1a.jx.internal.cloudapp.net>
2022-03-07 06:26:21 +0000

    run eval

--- run_eval_full.py
+++ run_eval_full.py
@@ -7,13 +7,9 @@ name = 'tldr_full'
 checkpoint = 'tldr_lr3e-05_seed161_full_datasetTrue/checkpoint-4'
 for dataset in datasets:
     os.system(f"python evaluate_bart.py bart-large-cnn-finetuned/{checkpoint}/ \
-<<<<<<< HEAD
-            --test_file data/{dataset}/{dataset}.csv \
-=======
             --test_file data/{dataset}/{dataset}_test.csv \
->>>>>>> ebf225522d74eee662e869d6d7f5ba0aca04a277
             --out_file results/{experiment}/preds_{name}_on_{dataset}.txt \
-            --ref_file data/reference/reference_{dataset}_full.txt \
+            --ref_file data/reference/reference_{dataset}_test.txt \
             --result_file results/{experiment}/rouge_{name}_on_{dataset}.csv")
 
 datasets = ['tldr', 'small_billsum', 'tiny_billsum']
@@ -21,13 +17,9 @@ name = 'tosdr_full'
 checkpoint = 'tosdr_lr3e-05_seed161_full_datasetTrue/checkpoint-8'
 for dataset in datasets:
     os.system(f"python evaluate_bart.py bart-large-cnn-finetuned/{checkpoint}/ \
-<<<<<<< HEAD
-            --test_file data/{dataset}/{dataset}.csv \
-=======
             --test_file data/{dataset}/{dataset}_test.csv \
->>>>>>> ebf225522d74eee662e869d6d7f5ba0aca04a277
             --out_file results/{experiment}/preds_{name}_on_{dataset}.txt \
-            --ref_file data/reference/reference_{dataset}_full.txt \
+            --ref_file data/reference/reference_{dataset}_test.txt \
             --result_file results/{experiment}/rouge_{name}_on_{dataset}.csv")
 
 datasets = ['tosdr', 'tldr', 'tiny_billsum']
@@ -35,13 +27,9 @@ name = 'small_billsum_full'
 checkpoint = 'small_billsum_lr3e-05_seed224_full_datasetTrue/checkpoint-52'
 for dataset in datasets:
     os.system(f"python evaluate_bart.py bart-large-cnn-finetuned/{checkpoint}/ \
-<<<<<<< HEAD
-            --test_file data/{dataset}/{dataset}.csv \
-=======
             --test_file data/{dataset}/{dataset}_test.csv \
->>>>>>> ebf225522d74eee662e869d6d7f5ba0aca04a277
             --out_file results/{experiment}/preds_{name}_on_{dataset}.txt \
-            --ref_file data/reference/reference_{dataset}_full.txt \
+            --ref_file data/reference/reference_{dataset}_test.txt \
             --result_file results/{experiment}/rouge_{name}_on_{dataset}.csv")
 
 
